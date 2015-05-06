6ca2e2a5045e7da8dc07b5a465cdd9f283db7ad1
makemeon <michele@hotmail.de>
2015-05-06 20:36:06 +0200

    TimeDataSource fertiggestellt

--- app/src/main/java/de/szut/ita13/app/schulapp/timetable/entity/Time.java
+++ app/src/main/java/de/szut/ita13/app/schulapp/timetable/entity/Time.java
@@ -5,7 +5,7 @@ package de.szut.ita13.app.schulapp.timetable.entity;
  */
 public class Time implements Table {
 
-    private Integer id;
+    private Long id;
 
     private String start;
 
@@ -13,11 +13,11 @@ public class Time implements Table {
 
     private String pause;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
